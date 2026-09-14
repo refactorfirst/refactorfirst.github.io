a795e845c82083e73e7faa5786e7389915af4cdc
Jim Bethancourt <jimbethancourt@gmail.com>
2026-09-14 18:50:23 -0500

    Fixed broken test

--- js/renderer.js
+++ js/renderer.js
@@ -18,13 +18,24 @@ export function renderTemplate(template, data) {
   const mustache = initializeMustache();
   const rendered = mustache.render(template, data);
   return DOMPurify.sanitize(rendered, {
-    USE_PROFILES: {
-      html: true,
-      svg: false,
-      svgFilters: false,
-      mathMl: false
-    },
+    ALLOWED_TAGS: [
+      'a', 'abbr', 'acronym', 'address', 'area', 'article', 'aside', 'audio',
+      'b', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas',
+      'caption', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd',
+      'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed',
+      'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3',
+      'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe',
+      'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link',
+      'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav',
+      'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param',
+      'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp',
+      'section', 'select', 'small', 'source', 'span', 'strong', 'style',
+      'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea',
+      'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var',
+      'video', 'wbr', '#text'
+    ],
     ADD_ATTR: ['target', 'align'],
-    FORBID_TAGS: ['script']
+    FORBID_TAGS: ['script'],
+    FORCE_BODY: true
   });
 }
